318bc0b6235a941225928c7bbb3d2e931b75d962
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-11 19:02:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for (var i = 0; i < musicians.length; i++){
+    var msgString = `${musicians[i]} plays ${instruments[i]}`;
+    arr.push(msgString);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while (i < facts.length){
+    facts[i] = `${facts[i]}!!!`;
+
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number){
+  var loveArray = [];
+
+  do {
+    loveArray.push("I love the Beatles!");
+    number++;
+  }
+  while (number < 15);
+  return loveArray;
+}
