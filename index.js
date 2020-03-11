7ce1f9dd66e3bc22f20355eb5d753cf9e2ca6cce
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-11 18:42:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function theBeatlesPlay(musicians, instruments){
   var arr = [];
-  for (i = 0; i < musicians.length; i++){
+  for (var i = 0; i < musicians.length; i++){
     var msgString = `${musicians[i]} plays ${instruments[i]}`;
     arr.push(msgString);
   }
